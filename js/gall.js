7e49f93d73ab4a8de3781ab8177aee13523e94c5
Kostas <kostas.sliazas@gmail.com>
2021-03-24 01:03:12 +0200

    Add files via upload

--- js/gall.js
+++ js/gall.js
@@ -1,244 +1,242 @@
-"use strict";
+'use strict'
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+function _classCallCheck (instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function') } }
 
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+function _defineProperties (target, props) { for (let i = 0; i < props.length; i++) { const descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor) } }
 
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
-
-;
+function _createClass (Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor }
 
 (function (w, d) {
-  'use strict';
+  'use strict'
 
-  var CreateUI = /*#__PURE__*/function () {
-    function CreateUI(container) {
-      _classCallCheck(this, CreateUI);
+  const CreateUI = /* #__PURE__ */(function () {
+    function CreateUI (container) {
+      _classCallCheck(this, CreateUI)
 
-      this.container = container;
-      this.indexOfImage = 0;
-      this.isActive = false;
-      this.isAutoplayOn = false;
-      this.frag = d.createDocumentFragment();
-      this.imag = d.createElement('div');
-      this.cent = d.createElement('div');
-      this.left = d.createElement('div');
-      this.rigt = d.createElement('div');
-      this.clos = d.createElement('div');
-      this.head = d.createElement('div');
-      this.foot = d.createElement('div');
-      this.alts = d.createElement('span');
-      this.play = d.createElement('div');
-      this.down = d.createElement('div');
-      this.onow = d.createElement('div');
-      this.imgs = d.createElement('img');
-      this.fine = d.createElement('span');
-      this.imgs.src = 'data:,';
-      this.imgs.setAttribute('alt', '');
-      this.imag.appendChild(this.head);
-      this.imag.appendChild(this.clos);
-      this.imag.appendChild(this.cent).appendChild(this.imgs);
-      this.head.appendChild(this.alts);
-      this.cent.appendChild(this.rigt);
-      this.cent.appendChild(this.left);
-      this.frag.appendChild(this.imag);
-      this.imag.appendChild(this.foot).appendChild(this.play);
-      this.foot.appendChild(this.onow).appendChild(this.down);
-      this.foot.appendChild(this.fine);
-      this.alts.id = 'alts';
-      this.head.id = 'head';
-      this.foot.id = 'foot';
-      this.clos.id = 'clos';
-      this.rigt.id = 'rigt';
-      this.left.id = 'left';
-      this.play.id = 'play';
-      this.onow.id = 'onow';
-      this.down.id = 'down';
-      this.cent.id = 'cent';
-      this.imag.id = 'imag';
-      this.imag.className = 'vis hidden';
-      d.body.appendChild(this.frag);
-      this.tim = 0;
-      this.imagesContainersArray = []; // get all images containers
+      this.container = container
+      this.indexOfImage = 0
+      this.isActive = false
+      this.isAutoplayOn = false
+      this.frag = d.createDocumentFragment()
+      this.imag = d.createElement('div')
+      this.cent = d.createElement('div')
+      this.left = d.createElement('div')
+      this.rigt = d.createElement('div')
+      this.clos = d.createElement('div')
+      this.head = d.createElement('div')
+      this.foot = d.createElement('div')
+      this.alts = d.createElement('span')
+      this.play = d.createElement('div')
+      this.down = d.createElement('div')
+      this.onow = d.createElement('div')
+      this.imgs = d.createElement('img')
+      this.fine = d.createElement('span')
+      this.imgs.src = 'data:,'
+      this.imgs.setAttribute('alt', '')
+      this.imag.appendChild(this.head)
+      this.imag.appendChild(this.clos)
+      this.imag.appendChild(this.cent).appendChild(this.imgs)
+      this.head.appendChild(this.alts)
+      this.cent.appendChild(this.rigt)
+      this.cent.appendChild(this.left)
+      this.frag.appendChild(this.imag)
+      this.imag.appendChild(this.foot).appendChild(this.play)
+      this.foot.appendChild(this.onow).appendChild(this.down)
+      this.foot.appendChild(this.fine)
+      this.alts.id = 'alts'
+      this.head.id = 'head'
+      this.foot.id = 'foot'
+      this.clos.id = 'clos'
+      this.rigt.id = 'rigt'
+      this.left.id = 'left'
+      this.play.id = 'play'
+      this.onow.id = 'onow'
+      this.down.id = 'down'
+      this.cent.id = 'cent'
+      this.imag.id = 'imag'
+      this.imag.className = 'vis hidden'
+      d.body.appendChild(this.frag)
+      this.tim = 0
+      this.imagesContainersArray = [] // get all images containers
 
-      this.imagesArray = []; // get all images
+      this.imagesArray = [] // get all images
 
-      for (var i = this.container.length; i--;) {
-        this.imagesContainersArray.push(this.container[i]);
+      for (let i = this.container.length; i--;) {
+        this.imagesContainersArray.push(this.container[i])
       }
 
-      for (var _i = this.imagesContainersArray.length; _i--;) {
-        var img = this.imagesContainersArray[_i].getElementsByTagName('img');
+      for (let _i = this.imagesContainersArray.length; _i--;) {
+        const img = this.imagesContainersArray[_i].getElementsByTagName('img')
 
-        for (var j = 0; j < img.length; j++) {
-          this.imagesArray.push(img[j]);
+        for (let j = 0; j < img.length; j++) {
+          this.imagesArray.push(img[j])
         }
       }
     }
 
     _createClass(CreateUI, [{
-      key: "autoPlay",
-      value: function autoPlay() {
-        var that = this;
+      key: 'autoPlay',
+      value: function autoPlay () {
+        const that = this
 
         if (that.indexOfImage === that.imagesArray.length - 1) {
-          that.indexOfImage = -1; // reset from first image to show
+          that.indexOfImage = -1 // reset from first image to show
         }
 
         if (this.isAutoplayOn) {
-          this.clear();
+          this.clear()
         } else {
-          this.isAutoplayOn = true;
-          this.play.className += ' acts';
+          this.isAutoplayOn = true
+          this.play.className += ' acts'
 
-          function delay() {
-            that.right();
-            that.show();
+          function delay () {
+            that.right()
+            that.show()
             that.tim = setTimeout(function () {
-              clearTimeout(that.tim);
-              that.tim = 0;
+              clearTimeout(that.tim)
+              that.tim = 0
 
               if (that.indexOfImage < that.imagesArray.length - 1) {
-                delay();
+                delay()
               }
-            }, 1000);
+            }, 1000)
 
             if (that.indexOfImage === that.imagesArray.length - 1) {
-              that.clear();
+              that.clear()
             }
           }
 
-          delay();
+          delay()
         }
       }
     }, {
-      key: "clear",
-      value: function clear() {
-        clearTimeout(this.tim);
-        this.tim = 0;
-        this.play.className = 'play';
-        this.isActive = false;
-        this.isAutoplayOn = false;
+      key: 'clear',
+      value: function clear () {
+        clearTimeout(this.tim)
+        this.tim = 0
+        this.play.className = 'play'
+        this.isActive = false
+        this.isAutoplayOn = false
       }
     }, {
-      key: "download",
-      value: function download() {
-        var a = d.createElement('a');
-        a.setAttribute('rel', 'noopener noreferrer');
-        a.target = '_blank';
-        a.href = this.imagesArray[this.indexOfImage].src;
-        a.download = this.imagesArray[this.indexOfImage].src.split('/').pop();
-        a.click();
-        a.remove();
-        this.onow.dataset.selected = this.imagesArray[this.indexOfImage].src;
+      key: 'download',
+      value: function download () {
+        const a = d.createElement('a')
+        a.setAttribute('rel', 'noopener noreferrer')
+        a.target = '_blank'
+        a.href = this.imagesArray[this.indexOfImage].src
+        a.download = this.imagesArray[this.indexOfImage].src.split('/').pop()
+        a.click()
+        a.remove()
+        this.onow.dataset.selected = this.imagesArray[this.indexOfImage].src
       }
     }, {
-      key: "lefts",
-      value: function lefts() {
+      key: 'lefts',
+      value: function lefts () {
         if (this.indexOfImage > 0) {
-          this.indexOfImage--;
+          this.indexOfImage--
         }
       }
     }, {
-      key: "right",
-      value: function right() {
+      key: 'right',
+      value: function right () {
         if (this.indexOfImage < this.imagesArray.length - 1) {
-          this.indexOfImage++;
+          this.indexOfImage++
         }
       }
     }, {
-      key: "close",
-      value: function close() {
-        this.clear();
-        d.body.style.overflow = 'visible';
-        this.imag.className += ' hidden';
+      key: 'close',
+      value: function close () {
+        this.clear()
+        d.body.style.overflow = 'visible'
+        this.imag.className += ' hidden'
       }
     }, {
-      key: "show",
-      value: function show() {
+      key: 'show',
+      value: function show () {
         if (!this.isActive) {
-          this.isActive = true;
-          this.imag.className = 'vis';
+          this.isActive = true
+          this.imag.className = 'vis'
           w.setTimeout(function () {
-            d.body.style.overflow = 'hidden';
-          }, 120); // delay for body overflow (animation fadeIn/out time + 50ms)
+            d.body.style.overflow = 'hidden'
+          }, 120) // delay for body overflow (animation fadeIn/out time + 50ms)
         }
 
-        this.left.className = this.indexOfImage === 0 ? 'left hidden' : 'left';
-        this.rigt.className = this.indexOfImage === this.imagesArray.length - 1 ? 'rigt hidden' : 'rigt';
-        this.alts.innerText = this.imagesArray[this.indexOfImage].getAttribute('alt') || '';
-        this.fine.innerText = Number(this.indexOfImage + 1) + '/' + this.imagesArray.length;
-        this.imgs.src = this.imagesArray[this.indexOfImage].src;
+        this.left.className = this.indexOfImage === 0 ? 'left hidden' : 'left'
+        this.rigt.className = this.indexOfImage === this.imagesArray.length - 1 ? 'rigt hidden' : 'rigt'
+        this.alts.innerText = this.imagesArray[this.indexOfImage].getAttribute('alt') || ''
+        this.fine.innerText = Number(this.indexOfImage + 1) + '/' + this.imagesArray.length
+        this.imgs.src = this.imagesArray[this.indexOfImage].src
       }
-    }]);
+    }])
 
-    return CreateUI;
-  }();
+    return CreateUI
+  }())
 
   d.addEventListener('DOMContentLoaded', function () {
-    var cont = d.getElementsByClassName('images-container')[0] ? d.getElementsByClassName('images-container') : [d.body]; // check and set any container default = body
+    const cont = d.getElementsByClassName('images-container')[0] ? d.getElementsByClassName('images-container') : [d.body] // check and set any container default = body
 
-    var images = new CreateUI(cont); // create UI for gallery slides after all DOM Loaded
+    const images = new CreateUI(cont) // create UI for gallery slides after all DOM Loaded
 
     d.addEventListener('click', function (e) {
-      e.preventDefault(); // prevent for default browser actions
+      e.preventDefault() // prevent for default browser actions
 
-      e.stopPropagation();
+      e.stopPropagation()
 
       switch (e.target.id || e.target.tagName) {
         case 'IMG':
-          images.indexOfImage = images.imagesArray.indexOf(e.target); // set image index on click
+          images.indexOfImage = images.imagesArray.indexOf(e.target) // set image index on click
 
-          images.show();
-          break;
+          images.show()
+          break
 
         case 'rigt':
-          images.clear();
-          images.right();
-          images.show();
-          break;
+          images.clear()
+          images.right()
+          images.show()
+          break
 
         case 'left':
-          images.clear();
-          images.lefts();
-          images.show();
-          break;
+          images.clear()
+          images.lefts()
+          images.show()
+          break
 
         case 'play':
-          images.autoPlay();
-          break;
+          images.autoPlay()
+          break
 
         case 'onow':
-          if (images.imagesArray[images.indexOfImage].src === images.onow.dataset.selected) return;
-          images.download();
-          break;
+          if (images.imagesArray[images.indexOfImage].src === images.onow.dataset.selected) return
+          images.download()
+          break
 
         case 'clos':
-          images.close();
-          break;
+          images.close()
+          break
       }
 
-      return false;
-    }); // add event on window listen for keys
+      return false
+    }) // add event on window listen for keys
 
     w.addEventListener('keyup', function (e) {
-      if (e.isComposing || e.key === 229) return;
-      e.preventDefault();
+      if (e.isComposing || e.key === 229) return
+      e.preventDefault()
 
       if (e.key === 'ArrowLeft') {
-        images.clear();
-        images.lefts();
-        images.show();
+        images.clear()
+        images.lefts()
+        images.show()
       }
 
       if (e.key === 'ArrowRight') {
-        images.clear();
-        images.right();
-        images.show();
+        images.clear()
+        images.right()
+        images.show()
       }
 
-      e.key === 'Escape' && images.close();
-      e.key === ' ' && images.autoPlay();
-    });
-  }); // w.imagges = CreateUI
-})(window, document);
+      e.key === 'Escape' && images.close()
+      e.key === ' ' && images.autoPlay()
+    })
+  }) // w.imagges = CreateUI
+})(window, document)
