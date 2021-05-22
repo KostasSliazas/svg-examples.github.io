badb8be89ffc5b90571c4faf0202cbef3f61d316
Kostas <kostas.sliazas@gmail.com>
2021-05-22 22:22:13 +0300

    Add files via upload

--- js/gall7.js
+++ js/gall7.js
@@ -1,12 +1,13 @@
-;
 (function (w, d) {
   'use strict'
+
   d.addEventListener('DOMContentLoaded', function () {
     const CreateUI = Object.create(null)
-
     CreateUI.tim = 0
     CreateUI.CreateUIContainersArray = [] // get all CreateUI containers
+
     CreateUI.CreateUIArray = [] // get all CreateUI
+
     CreateUI.container = null
     CreateUI.indexOfImage = 0
     CreateUI.isActive = false
@@ -72,6 +73,7 @@
         const that = this
         that.tim = setTimeout(function () {
           that.right().show()
+
           if (that.indexOfImage === that.CreateUIArray.length - 1) {
             that.clear()
           }
@@ -81,12 +83,15 @@
 
     CreateUI.loaded = function (e) {
       const that = this
+
       e.onload = function (e) {
         e.target.parentElement.className = ''
+
         if (that.isAutoplayOn) {
           that.autoplayLoop()
         }
       }
+
       const img = e
       e.src = img.src
     }
@@ -113,15 +118,13 @@
     }
 
     CreateUI.lefts = function () {
-      if (this.indexOfImage > 0) this.indexOfImage--
-      else this.indexOfImage = this.CreateUIArray.length - 1
+      if (this.indexOfImage > 0) this.indexOfImage--; else this.indexOfImage = this.CreateUIArray.length - 1
       this.setActiveClass.call(this.ilef)
       return this
     }
 
     CreateUI.right = function () {
-      if (this.indexOfImage < this.CreateUIArray.length - 1) this.indexOfImage++
-      else this.indexOfImage = 0
+      if (this.indexOfImage < this.CreateUIArray.length - 1) this.indexOfImage++; else this.indexOfImage = 0
       this.setActiveClass.call(this.irig)
       return this
     }
@@ -143,46 +146,47 @@
 
     CreateUI.show = function () {
       // if null is set as index return false
-      if (this.indexOfImage === null) return false
+      if (this.indexOfImage === null) return false // don't rewrite values if active and set active gallery
 
-      // don't rewrite values if active and set active gallery
       if (!this.isActive) {
         this.isActive = true
         this.imag.className = ''
         d.body.style.overflowY = 'hidden'
         this.imag.focus()
       }
-      const image = this.CreateUIArray[this.indexOfImage]
 
-      // two lines below for hiding left right buttons
+      const image = this.CreateUIArray[this.indexOfImage] // two lines below for hiding left right buttons
+
       this.left.className = this.indexOfImage === 0 ? 'hide7' : ''
       this.rigt.className = this.indexOfImage === this.CreateUIArray.length - 1 ? 'hide7' : ''
-
       this.insi.className = 'spin7'
       this.alts.innerText = image.src.slice(image.src.lastIndexOf('/') + 1)
       this.fine.innerText = Number(this.indexOfImage) + 1 + '/' + this.CreateUIArray.length
+      let imageSrc // if svg load svg
 
-      let imageSrc
-      // if svg load svg
       if (image.src.substr(image.src.length - 3) === 'svg') {
         imageSrc = image.src
       } else {
         // chech is there a biger resolution image in 'big' folder
         imageSrc = image.src.substring(0, image.src.lastIndexOf('/') + 1) + 'big' + image.src.slice(image.src.lastIndexOf('/'))
       }
+
       this.imgs.onerror = function (e) {
         e.target.src = image.src
       }
+
       this.imgs.src = imageSrc
     }
 
     CreateUI.container = d.getElementsByClassName('CreateUI-container')[0] ? d.getElementsByClassName('CreateUI-container') : [d.body] // check and set any container default = body
+
     for (let i = CreateUI.container.length - 1; i >= 0; i--) {
       CreateUI.CreateUIContainersArray.push(CreateUI.container[i])
     }
 
-    for (let i = CreateUI.CreateUIContainersArray.length - 1; i >= 0; i--) {
-      const img = CreateUI.CreateUIContainersArray[i].getElementsByTagName('img')
+    for (let _i = CreateUI.CreateUIContainersArray.length - 1; _i >= 0; _i--) {
+      const img = CreateUI.CreateUIContainersArray[_i].getElementsByTagName('img')
+
       for (let j = 0; j < img.length; j++) {
         img[j].parentElement.className = 'spin7'
         CreateUI.loaded(img[j])
@@ -190,21 +194,23 @@
       }
     }
 
-    const listenForCreateUI = e => {
+    const listenForCreateUI = function listenForCreateUI (e) {
       if (e.target.tagName === 'IMG') {
         e.preventDefault()
         e.stopImmediatePropagation()
         CreateUI.indexOfImage = CreateUI.CreateUIArray.indexOf(e.target) === -1 ? null : CreateUI.CreateUIArray.indexOf(e.target) // set image index on click
+
         CreateUI.show()
       }
     }
 
     if (CreateUI.CreateUIContainersArray[0].tagName === 'BODY') {
       CreateUI.CreateUIArray.pop() // remove last element from array if body is selected
+
       d.addEventListener('click', listenForCreateUI)
     } else {
-      for (let i = CreateUI.CreateUIContainersArray.length - 1; i >= 0; i--) {
-        CreateUI.CreateUIContainersArray[i].addEventListener('click', listenForCreateUI)
+      for (let _i2 = CreateUI.CreateUIContainersArray.length - 1; _i2 >= 0; _i2--) {
+        CreateUI.CreateUIContainersArray[_i2].addEventListener('click', listenForCreateUI)
       }
     }
 
@@ -215,13 +221,14 @@
       e.target.id === 'left7' && CreateUI.clear().lefts().show()
       e.target.id === 'play7' && CreateUI.autoPlay()
       e.target.id === 'clos7' && CreateUI.close()
+
       if (e.target.id === 'wdow7') {
         if (CreateUI.CreateUIArray[CreateUI.indexOfImage].src === CreateUI.onow.dataset.selected) return
         CreateUI.clear().downloads()
       }
+
       return false
     })
-
     w.addEventListener('keyup', function (e) {
       if (!CreateUI.isActive || e.isComposing || e.key === 229) return false
       e.key === 'ArrowLeft' && CreateUI.clear().lefts().show()
