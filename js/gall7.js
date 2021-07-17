a4cf4fb2a909e7070023704ecfc954a1a8782889
Kostas <kostas.sliazas@gmail.com>
2021-07-17 14:08:04 +0300

    Add files via upload

--- js/gall7.js
+++ js/gall7.js
@@ -1,68 +1,83 @@
 (function (w, d) {
   'use strict'
-  const CreateUI = Object.create(null)
+  const IG = Object.create(null)
+
+  // imageGalleryConfig variables
+  const getConfig = typeof w.imageGalleryConfig !== 'undefined' && w.imageGalleryConfig
+  IG.folder = typeof getConfig.folder === 'string' ? getConfig.folder : 'big' // default folder 'big'
+  IG.imageContainer = getConfig.imageContainer || 'images-container' // default class for images
+  IG.timer = typeof getConfig.delaySeconds === 'number' && isFinite(getConfig.delaySeconds) ? getConfig.delaySeconds * 1000 : 2000 // delay for image auto showing
+  IG.showButtons = !(typeof getConfig.showButtons === 'boolean' && !getConfig.showButtons)
   // create all elements containers array
-  CreateUI.createUIContainersArray = []
+  IG.IGContainersArray = []
   // create all elements array
-  CreateUI.createUIArray = []
+  IG.IGArray = []
   // initialize container with null value
-  CreateUI.container = null
-  CreateUI.indexOfImage = 0
-  CreateUI.isActive = false
-  CreateUI.isAutoplayOn = false
-  CreateUI.timeOutVar = 0
+  IG.container = null
+  IG.indexOfImage = 0
+  IG.isActive = false
+  IG.isAutoplayOn = false
+  IG.timeOutVar = 0
   // all stuff for creating main gallery window
-  CreateUI.frag = d.createDocumentFragment()
-  CreateUI.clos = d.createElement('button')
-  CreateUI.ilef = d.createElement('button')
-  CreateUI.irig = d.createElement('button')
-  CreateUI.wdow = d.createElement('button')
-  CreateUI.imag = d.createElement('div')
-  CreateUI.cent = d.createElement('div')
-  CreateUI.left = d.createElement('div')
-  CreateUI.rigt = d.createElement('div')
-  CreateUI.foot = d.createElement('div')
-  CreateUI.alts = d.createElement('div')
-  CreateUI.play = d.createElement('div')
-  CreateUI.onow = d.createElement('div')
-  CreateUI.fine = d.createElement('div')
-  CreateUI.head = d.createElement('div')
-  CreateUI.insi = d.createElement('div')
-  CreateUI.imgs = d.createElement('img')
-  CreateUI.down = d.createElement('span')
-  CreateUI.imag.appendChild(CreateUI.head).id = 'head7'
-  CreateUI.imag.appendChild(CreateUI.clos).id = 'clos7'
-  CreateUI.imag.appendChild(CreateUI.cent).id = 'cent7'
-  CreateUI.onow.appendChild(CreateUI.alts).id = 'alts7'
-  CreateUI.frag.appendChild(CreateUI.imag).id = 'imag7'
-  CreateUI.cent.appendChild(CreateUI.rigt).appendChild(CreateUI.irig).id = 'irig7'
-  CreateUI.cent.appendChild(CreateUI.insi).appendChild(CreateUI.imgs)
-  CreateUI.cent.appendChild(CreateUI.left).appendChild(CreateUI.ilef).id = 'ilef7'
-  CreateUI.imag.appendChild(CreateUI.foot).appendChild(CreateUI.play).id = 'play7'
-  CreateUI.imag.appendChild(CreateUI.onow).appendChild(CreateUI.wdow).appendChild(CreateUI.down)
-  CreateUI.foot.appendChild(CreateUI.fine)
-  CreateUI.foot.id = 'foot7'
-  CreateUI.rigt.id = 'rigt7'
-  CreateUI.insi.id = 'insi7'
-  CreateUI.left.id = 'left7'
-  CreateUI.onow.id = 'onow7'
-  CreateUI.down.id = 'down7'
-  CreateUI.wdow.id = 'wdow7'
-  CreateUI.clos.setAttribute('title', 'Press Esc to close')
-  CreateUI.imgs.setAttribute('alt', '')
-  CreateUI.imag.setAttribute('tabindex', '-1')
-  CreateUI.imag.className = 'hide7'
+  IG.frag = d.createDocumentFragment()
+  IG.clos = d.createElement('button')
+  IG.ilef = d.createElement('button')
+  IG.irig = d.createElement('button')
+  IG.imag = d.createElement('div')
+  IG.cent = d.createElement('div')
+  IG.left = d.createElement('div')
+  IG.rigt = d.createElement('div')
+  IG.head = d.createElement('div')
+  IG.insi = d.createElement('div')
+
+  IG.cent.appendChild(IG.rigt).appendChild(IG.irig).id = 'irig7'
+  IG.cent.appendChild(IG.left).appendChild(IG.ilef).id = 'ilef7'
+  IG.imag.appendChild(IG.head).appendChild(IG.clos).id = 'clos7'
+  IG.imag.appendChild(IG.cent).id = 'cent7'
+  IG.frag.appendChild(IG.imag).id = 'imag7'
+  IG.rigt.id = 'rigt7'
+  IG.insi.id = 'insi7'
+  IG.left.id = 'left7'
+  IG.head.id = 'head7'
+  if (IG.showButtons) {
+    IG.wdow = d.createElement('button')
+    IG.play = d.createElement('button')
+    IG.foot = d.createElement('div')
+    IG.alts = d.createElement('div')
+    IG.onow = d.createElement('div')
+    IG.fine = d.createElement('div')
+    IG.down = d.createElement('span')
+    IG.foot.id = 'foot7'
+    IG.onow.id = 'onow7'
+    IG.down.id = 'down7'
+    IG.wdow.id = 'wdow7'
+    IG.fine.id = 'stat7'
+    IG.onow.appendChild(IG.alts).id = 'alts7'
+    IG.imag.appendChild(IG.foot).appendChild(IG.play).id = 'play7'
+    IG.imag.appendChild(IG.onow).appendChild(IG.wdow).appendChild(IG.down)
+    IG.foot.appendChild(IG.fine)
+  }
+
+  IG.cent.appendChild(IG.insi)
+  IG.imgs = d.createElement('img')
+  IG.cent.appendChild(IG.insi).appendChild(IG.imgs)
+  IG.imgs.setAttribute('alt', '')
   // create empty image source
-  CreateUI.imgs.src = 'data:,'
+  IG.imgs.src = 'data:,'
+
+  IG.clos.setAttribute('title', 'Press Esc to close')
+  IG.imag.setAttribute('tabindex', '-1')
+  IG.imag.className = 'hide7'
+
   // append document fragment to BODY
-  d.body.appendChild(CreateUI.frag)
+  d.body.appendChild(IG.frag)
 
   // autoplay method
-  CreateUI.autoPlay = function () {
+  IG.autoPlay = function () {
     if (this.isAutoplayOn) {
       this.clear()
     } else {
-      this.play.className = 'acts7'
+      if (IG.showButtons) this.play.className = 'acts7'
       this.isAutoplayOn = true
       this.setActiveClass.call(this.play)
       this.loaded.call(this.imgs)
@@ -70,34 +85,34 @@
   }
 
   // autoplay method loop
-  CreateUI.autoPlayLoop = function () {
+  IG.autoPlayLoop = function () {
     this.timeOutVar = setTimeout(function () {
       this.right().show()
-      if (this.indexOfImage === this.createUIArray.length - 1) this.clear()
-    }.bind(this), 1300)
+      if (this.indexOfImage === this.IGArray.length - 1) this.clear()
+    }.bind(this), this.timer)
   }
 
   // image is loaded method
-  CreateUI.loaded = function () {
+  IG.loaded = function () {
     this.onload = loadComplete.bind(this)
-    const img = this
-    this.src = img.src
+    const src = this
+    this.src = src.src
   }
   // call method on image element
-  CreateUI.loaded.call(CreateUI.imgs)
+  // IG.loaded.call(IG.imgs)
 
   // clear method to reset all values
-  CreateUI.clear = function () {
+  IG.clear = function () {
     clearTimeout(this.timeOutVar)
     this.timeOutVar = 0
     this.isAutoplayOn = false
     this.isLoadedImage = false
-    this.play.classList.remove('acts7')
+    if (IG.showButtons) this.play.classList.remove('acts7')
     return this
   }
 
   // downloads method
-  CreateUI.downloads = function () {
+  IG.downloads = function () {
     // add class active for button animation
     this.setActiveClass.call(this.down)
     const a = d.createElement('a')
@@ -111,123 +126,135 @@
   }
 
   // to left button method loop from images index
-  CreateUI.lefts = function () {
+  IG.lefts = function () {
     if (this.indexOfImage > 0) this.indexOfImage--
-    else this.indexOfImage = this.createUIArray.length - 1
+    else this.indexOfImage = this.IGArray.length - 1
     this.setActiveClass.call(this.ilef)
     return this
   }
 
   // to right button method loop from images index
-  CreateUI.right = function () {
-    if (this.indexOfImage < this.createUIArray.length - 1) this.indexOfImage++
+  IG.right = function () {
+    if (this.indexOfImage < this.IGArray.length - 1) this.indexOfImage++
     else this.indexOfImage = 0
     this.setActiveClass.call(this.irig)
     return this
   }
 
   // function on close
-  CreateUI.close = function () {
+  IG.close = function () {
     this.clear()
     this.isActive = false
     this.imag.className = 'hide7'
     d.body.style.overflowY = 'visible'
+    d.children[0].style.overflow = 'visible'
   }
 
   // add class for button animation (when clicked)
-  CreateUI.setActiveClass = function () {
-    this.classList.add('focu7')
-    setTimeout(function () {
-      this.classList.remove('focu7')
-    }.bind(this), 150)
+  IG.setActiveClass = function () {
+    if (typeof this !== 'undefined') {
+      this.classList.add('focu7')
+      setTimeout(function () {
+        this.classList.remove('focu7')
+      }.bind(this), 150)
+    }
   }
 
   // show image method to show image when loaded
-  CreateUI.show = function () {
+  IG.show = function () {
+    this.insi.removeChild(this.insi.firstChild)
+    // creating image again for UX to show that image is loading
+    this.imgs = d.createElement('img')
+    this.loaded.call(this.imgs)
+    // this.imgs.setAttribute('alt', '')
+    this.insi.appendChild(this.imgs)
     // if null is set as index return false
     if (this.indexOfImage === null) return false
     this.insi.className = 'spin7'
-    const image = this.createUIArray[this.indexOfImage]
+    const image = this.IGArray[this.indexOfImage]
     // don't rewrite values if active and set active gallery
     if (!this.isActive) {
       this.isActive = true
       d.body.style.overflowY = 'hidden'
+      d.children[0].style.overflow = 'hidden'
       this.imag.className = ''
       this.imag.focus()
     }
     // two lines below for hiding left right buttons
     this.left.className = this.indexOfImage === 0 ? 'hide7' : ''
-    this.rigt.className = this.indexOfImage === this.createUIArray.length - 1 ? 'hide7' : ''
-    this.alts.innerText = decodeURI(image.src.slice(image.src.lastIndexOf('/') + 1))
-    this.fine.innerText = Number(this.indexOfImage) + 1 + '/' + this.createUIArray.length
+    this.rigt.className = this.indexOfImage === this.IGArray.length - 1 ? 'hide7' : ''
+    if (IG.showButtons) {
+      this.alts.innerText = decodeURI(image.src.slice(image.src.lastIndexOf('/') + 1))
+      this.fine.innerText = Number(this.indexOfImage) + 1 + '/' + this.IGArray.length
+    }
     this.imgs.onerror = function (e) { e.target.src = image.src }
-    this.imgs.src = image.src.substr(image.src.length - 3) === 'svg' ? image.src : image.src.substring(0, image.src.lastIndexOf('/') + 1) + 'big' + image.src.slice(image.src.lastIndexOf('/'))
+    this.imgs.src = image.src.substr(image.src.length - 3) === 'svg' ? image.src : image.src.substring(0, image.src.lastIndexOf('/') + 1) + this.folder + image.src.slice(image.src.lastIndexOf('/'))
   }
-
   // asign container elements or BODY (default = BODY)
-  CreateUI.container = d.getElementsByClassName('images-container')[0]
-    ? d.getElementsByClassName('images-container')
+  IG.container = d.getElementsByClassName(IG.imageContainer).length > 0
+    ? d.getElementsByClassName(IG.imageContainer)
     : d.getElementsByTagName('body')
-  for (let l = CreateUI.container.length - 1; l >= 0; l--) {
-    CreateUI.createUIContainersArray.push(CreateUI.container[l])
+  for (let l = IG.container.length - 1; l >= 0; l--) {
+    IG.IGContainersArray.push(IG.container[l])
   }
-
   // Loop from elements
-  for (let i = CreateUI.createUIContainersArray.length - 1; i >= 0; i--) {
-    const img = CreateUI.createUIContainersArray[i].getElementsByTagName('img')
+  for (let i = IG.IGContainersArray.length - 1; i >= 0; i--) {
+    const img = IG.IGContainersArray[i].getElementsByTagName('img')
     for (let j = 0; j < img.length; j++) {
       img[j].parentElement.classList.add('spin7')
-      CreateUI.loaded.call(img[j])
-      CreateUI.createUIArray.push(img[j])
+      IG.loaded.call(img[j])
+      IG.IGArray.push(img[j])
     }
   }
 
   // listen for clicked on image element and load show method
-  const listenForCreateUI = function listenForCreateUI (e) {
+  const listenForIG = function listenForIG (e) {
     if (e.target.tagName === 'IMG') {
       e.preventDefault()
       e.stopImmediatePropagation()
-      CreateUI.indexOfImage = CreateUI.createUIArray.indexOf(e.target) === -1 ? null : CreateUI.createUIArray.indexOf(e.target) // set image index on click
-      CreateUI.show()
+      IG.indexOfImage = IG.IGArray.indexOf(e.target) === -1 ? null : IG.IGArray.indexOf(e.target) // set image index on click
+      IG.show()
     }
   }
 
-  if (CreateUI.createUIContainersArray[0] && CreateUI.createUIContainersArray[0].tagName === 'BODY') {
+  if (IG.IGContainersArray[0] && IG.IGContainersArray[0].tagName === 'BODY') {
     // remove last element from array if body is selected
-    CreateUI.createUIArray.pop()
-    d.body.addEventListener('click', listenForCreateUI)
+    IG.IGArray.pop()
+    d.body.addEventListener('click', listenForIG)
   } else {
-    for (let k = CreateUI.createUIContainersArray.length - 1; k >= 0; k--) { CreateUI.createUIContainersArray[k].addEventListener('click', listenForCreateUI) }
+    for (let k = IG.IGContainersArray.length - 1; k >= 0; k--) { IG.IGContainersArray[k].addEventListener('click', listenForIG) }
   }
 
   // autoplay and image loaded helper to remove class 'loader'
   function loadComplete () {
-    this.parentElement.classList.remove('spin7')
-    CreateUI.isAutoplayOn && CreateUI.autoPlayLoop()
+    if (typeof this !== 'undefined' && this.parentElement) {
+      this.parentElement.classList.remove('spin7')
+      IG.isAutoplayOn && IG.autoPlayLoop()
+    }
   }
 
   // add click addEventListener to image div (gallery window)
-  CreateUI.imag.addEventListener('click', function (e) {
-    if (!CreateUI.isActive) return false
+  IG.imag.addEventListener('click', function (e) {
+    if (!IG.isActive) return false
     if (e.target.id === 'wdow7') {
-      if (CreateUI.createUIArray[CreateUI.indexOfImage].src === CreateUI.onow.dataset.selected) return false
-      CreateUI.clear().downloads()
+      if (IG.IGArray[IG.indexOfImage].src === IG.onow.dataset.selected) return false
+      IG.clear().downloads()
     }
-    e.target.id === 'rigt7' && CreateUI.clear().right().show()
-    e.target.id === 'left7' && CreateUI.clear().lefts().show()
-    e.target.id === 'play7' && CreateUI.autoPlay()
-    e.target.id === 'clos7' && CreateUI.close()
+    e.target.id === 'rigt7' && IG.clear().right().show()
+    e.target.id === 'left7' && IG.clear().lefts().show()
+    e.target.id === 'play7' && IG.autoPlay()
+    e.target.id === 'clos7' && IG.close()
     e.preventDefault()
     e.stopImmediatePropagation()
   })
 
   // add keyup addEventListener to image div (gallery window)
   w.addEventListener('keyup', function (e) {
-    if (!CreateUI.isActive || e.isComposing || e.key === 229) return false
-    e.key === 'ArrowLeft' && CreateUI.clear().lefts().show()
-    e.key === 'ArrowRight' && CreateUI.clear().right().show()
-    e.key === 'Escape' && CreateUI.close()
-    e.key === ' ' && CreateUI.autoPlay()
+    if (!IG.isActive || e.isComposing || e.key === 229) return false
+    e.key === 'ArrowLeft' && IG.clear().lefts().show()
+    e.key === 'ArrowRight' && IG.clear().right().show()
+    e.key === 'Escape' && IG.close()
+    e.key === ' ' && IG.autoPlay()
     e.preventDefault()
     e.stopImmediatePropagation()
   })
@@ -255,13 +282,13 @@
     const elapsedTime = endTime - startTime
     if (Math.abs(moveX) > minHorizontalMove && Math.abs(moveY) < maxVerticalMove && elapsedTime < withinMs) {
       if (moveX < 0) {
-        CreateUI.clear().right().show()
+        IG.clear().right().show()
       } else {
-        CreateUI.clear().lefts().show()
+        IG.clear().lefts().show()
       }
     }
   }
-  CreateUI.imag.addEventListener('touchstart', touchStart)
-  CreateUI.imag.addEventListener('touchend', touchEnd)
+  IG.imag.addEventListener('touchstart', touchStart, { passive: true })
+  IG.imag.addEventListener('touchend', touchEnd)
   // everything to handle swipe left/right ends
 })(window, document)
