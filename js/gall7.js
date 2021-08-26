6b1df197fbfc91111096f9c98707d6918997e271
Kostas <kostas.sliazas@gmail.com>
2021-08-26 18:51:11 +0300

    Add files via upload

--- js/gall7.js
+++ js/gall7.js
@@ -4,15 +4,15 @@
   // imageGalleryConfig variables with defaults
   // not using dot notation to not shorten variable names compile with google closure
   const getConfig = typeof w['imageGalleryConfig'] === 'undefined' || w['imageGalleryConfig']
-  IG.folder = getConfig['folder'] || 'big' // default folder 'big'
+  IG.folder = getConfig['folder'] || 'big/' // default folder 'big'
   IG.imageContainer = getConfig['imageContainer'] || 'images-container'
   IG.timer = typeof getConfig['delaySeconds'] === 'number' && isFinite(getConfig['delaySeconds']) ? getConfig['delaySeconds'] * 1000 : 2000
   IG.hideButtonsOnPlay = typeof getConfig['hideButtonsOnPlay'] === 'boolean' ? getConfig['hideButtonsOnPlay'] : false
   IG.showButtons = typeof getConfig['showButtons'] === 'boolean' ? getConfig['showButtons'] : true
   // elements containers array
-  IG.IGContainersArray = []
+  IG.containersArray = []
   // all elements array
-  IG.IGArray = []
+  IG.imagesArray = []
   // initialize container with null value
   IG.container = null
   IG.indexOfImage = 0
@@ -90,7 +90,7 @@
   IG.autoPlayLoop = function () {
     this.timeOutVar = setTimeout(function () {
       this.right().show()
-      if (this.indexOfImage === this.IGArray.length - 1) this.clear()
+      if (this.indexOfImage === this.imagesArray.length - 1) this.clear()
     }.bind(this), this.timer)
   }
 
@@ -122,20 +122,20 @@
     a.setAttribute('download', this.imgs.src)
     a.click()
     a.remove()
-    this.onow.dataset.selected = this.imgs.src
+    this.onow.dataset.selected = this.imgs.src.split('/').pop()
   }
 
   // to left button method loop from images index
   IG.lefts = function () {
     if (this.indexOfImage > 0) this.indexOfImage--
-    else this.indexOfImage = this.IGArray.length - 1
+    else this.indexOfImage = this.imagesArray.length - 1
     !this.isAutoplayOn && this.setActiveClass.call(this.ilef)
     return this
   }
 
   // to right button method loop from images index
   IG.right = function () {
-    if (this.indexOfImage < this.IGArray.length - 1) this.indexOfImage++
+    if (this.indexOfImage < this.imagesArray.length - 1) this.indexOfImage++
     else this.indexOfImage = 0
     !this.isAutoplayOn && this.setActiveClass.call(this.irig)
     return this
@@ -170,14 +170,14 @@
     }
     if (!this.isAutoplayOn || !this.hideButtonsOnPlay) {
       this.left.className = this.indexOfImage === 0 ? 'hide7' : ''
-      this.rigt.className = this.indexOfImage === this.IGArray.length - 1 ? 'hide7' : ''
+      this.rigt.className = this.indexOfImage === this.imagesArray.length - 1 ? 'hide7' : ''
     }
   }
 
   // show image method to show image when loaded
   IG.show = function () {
     // if null is set as index return false
-    if (this.indexOfImage === null) return false
+    if (this.indexOfImage < 0) return false
     // if image exist remove and later recreate it
     this.isLoadedImage && this.insi.removeChild(this.insi.firstChild)
     // don't rewrite values if active and set active gallery
@@ -194,17 +194,24 @@
     this.loaded.call(this.imgs)
     this.insi.appendChild(this.imgs)
     this.insi.className = 'spin7'
-    const image = this.IGArray[this.indexOfImage]
+    const image = this.imagesArray[this.indexOfImage]
     this.imgs.setAttribute('alt', image.getAttribute('alt'))
     this.hideButtons()
     if (this.showButtons) {
       this.alts.innerText = decodeURI(image.src.slice(image.src.lastIndexOf('/') + 1))
-      this.fine.innerText = Number(this.indexOfImage) + 1 + '/' + this.IGArray.length
+      this.fine.innerText = Number(this.indexOfImage) + 1 + '/' + this.imagesArray.length
     }
-    this.imgs.onerror = function (e) {
-      e.target.src = image.src
-    }
-    this.imgs.src = image.src.substr(image.src.length - 3) === 'svg' ? image.src : image.src.substring(0, image.src.lastIndexOf('/') + 1) + this.folder + image.src.slice(image.src.lastIndexOf('/'))
+
+    // get image src file name
+    const fileName = image.src.split('/').pop()
+    setTimeout(function () {
+      // if there is no big image return small image src
+      this.imgs.onerror = function (e) {
+        e.target.src = image.src
+      }
+      // if image src === svg return that image.src else try to load big image src
+      this.imgs.src = image.src.substr(image.src.length - 3) === 'svg' ? image.src : image.src.replace(fileName, this.folder + fileName)
+    }.bind(this), 0)
   }
 
   // asign container elements or BODY (default = BODY)
@@ -212,16 +219,18 @@
     ? d.getElementsByClassName(IG.imageContainer)
     : d.getElementsByTagName('body')
   for (let l = IG.container.length - 1; l >= 0; l--) {
-    IG.IGContainersArray.push(IG.container[l])
+    IG.containersArray.push(IG.container[l])
   }
 
   // Loop from elements and add to array
-  for (let i = IG.IGContainersArray.length - 1; i >= 0; i--) {
-    const img = IG.IGContainersArray[i].getElementsByTagName('img')
+  for (let i = IG.containersArray.length - 1; i >= 0; i--) {
+    const img = IG.containersArray[i].getElementsByTagName('img')
     for (let j = 0; j < img.length; j++) {
+      // this always overwrite parent class name if same parent
+      // if (!img[j].parentElement.classList.contains('spin7'))
       img[j].parentElement.classList.add('spin7')
       IG.loaded.call(img[j])
-      IG.IGArray.push(img[j])
+      IG.imagesArray.push(img[j])
     }
   }
 
@@ -230,16 +239,16 @@
     if (e.target.tagName === 'IMG') {
       e.preventDefault()
       e.stopImmediatePropagation()
-      IG.indexOfImage = IG.IGArray.indexOf(e.target) === -1 ? null : IG.IGArray.indexOf(e.target) // set image index on click
+      IG.indexOfImage = IG.imagesArray.indexOf(e.target) === -1 ? -1 : IG.imagesArray.indexOf(e.target) // set image index on click
       IG.show()
     }
   }
 
-  if (IG.IGContainersArray[0] && IG.IGContainersArray[0].tagName === 'BODY') {
+  if (IG.containersArray[0] && IG.containersArray[0].tagName === 'BODY') {
     d.body.addEventListener('click', listenForIG)
   } else {
-    for (let k = IG.IGContainersArray.length - 1; k >= 0; k--) {
-      IG.IGContainersArray[k].addEventListener('click', listenForIG)
+    for (let k = IG.containersArray.length - 1; k >= 0; k--) {
+      IG.containersArray[k].addEventListener('click', listenForIG)
     }
   }
 
@@ -254,10 +263,7 @@
   // add click addEventListener to image div (gallery window)
   IG.imag.addEventListener('click', function (e) {
     if (!IG.isActive) return false
-    if (e.target.id === 'wdow7') {
-      if (IG.IGArray[IG.indexOfImage].src === IG.onow.dataset.selected) return false
-      IG.clear().downloads()
-    }
+    if (e.target.id === 'wdow7' && IG.imagesArray[IG.indexOfImage].src.split('/').pop() !== IG.onow.dataset.selected) IG.clear().downloads()
     if (e.target.id === 'cent7' && IG.isAutoplayOn) IG.clear()
     e.target.id === 'rigt7' && IG.clear().right().show()
     e.target.id === 'left7' && IG.clear().lefts().show()
